fe151ec4ad1f4128cf94bede3966ae0548599a1a
Tom Holland <codesman@users.noreply.github.com>
2016-12-15 18:54:21 -0800

    Updated to Vapor 1.3

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
     ]
 )
